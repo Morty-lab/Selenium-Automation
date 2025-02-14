a944b61e58d052c432a7ffeb837f2f6ae42720ec
morty_lab <aniejosephcabahug@gmail.com>
2025-02-14 23:49:16 +0800

    working msotly just a little bug on the formating of the dictionary

--- automations/FacebookScraper.py
+++ automations/FacebookScraper.py
@@ -1,9 +1,13 @@
 import json
 from seleniumbase import BaseCase
+from datetime import date
+import re
+
 
 
 class FacebookScraper(BaseCase):
     def test_facebook_scraper(self):
+        self.maximize_window()
         video_urls = self.get_urls_from_json("video.json")
         post_urls = self.get_urls_from_json("post.json")
 
@@ -22,20 +26,24 @@ class FacebookScraper(BaseCase):
     #         executor.map(self.open_facebook_links, [video_urls, post_urls])
 
     def open_facebook_video_links(self, urls):
+        
         for url in urls:
             self.open(url)
+            company = self.get_text("div.xu06os2.x1ok221b")
             self.close_facebook_popups()
             self.sleep(1)
             self.change_visibility_to_all()
             self.sleep(2)
             self.expand_comments()
+            self.get_comments(company)
             self.sleep(2)
-        
+
     def close_facebook_popups(self):
         try:
             text_present = self.assert_text("See more on Facebook", "div[role='dialog']")
 
             if text_present and self.is_element_visible("[aria-label='Close'][role='button']"):
+                self.execute_script("arguments[0].scrollIntoView(false);", self.find_element("[aria-label='Close'][role='button']"))
                 self.click("[aria-label='Close'][role='button']")
                 self.sleep(1)
 
@@ -44,25 +52,59 @@ class FacebookScraper(BaseCase):
         
     def change_visibility_to_all(self):
         try:
+            self.execute_script("arguments[0].scrollIntoView(false);", self.find_element('span:contains("Most relevant")'))
             self.click('span:contains("Most relevant")')
             self.sleep(1)
+            self.execute_script("arguments[0].scrollIntoView(false);", self.find_element('div[role="menuitem"]:contains("Show all comments, including potential spam.")'))
             self.click('div[role="menuitem"]:contains("Show all comments, including potential spam.")')
-            self.sleep(3)
         except Exception as e:
             print(f"Unable to change comment visibility: {e}")
             
     def expand_comments(self):
-        view_more_button_visible = self.is_element_visible('span:contains("View more")')
-        if view_more_button_visible:
+        while self.is_element_visible('span:contains("View more")'):
+            self.execute_script("arguments[0].scrollIntoView(false);", self.find_element('span:contains("View more")'))
             self.click('span:contains("View more")')
             self.sleep(1)
-            self.execute_script("window.scrollTo(0, 0);")
-            comments_div = self.find_element("div.x78zum5.xdt5ytf.x6ikm8r.x1odjw0f.x1iyjqo2.x1pi30zi.x1swvt13")
-            self.execute_script("arguments[0].scrollIntoView(true);", comments_div)
+        else:
+            print("No more 'View more' buttons are visible.")
 
-        comments_div = self.find_element("div.x78zum5.xdt5ytf.x6ikm8r.x1odjw0f.x1iyjqo2.x1pi30zi.x1swvt13")
-        for _ in range(20):
-            self.execute_script("arguments[0].scrollBy(0, arguments[0].scrollHeight);", comments_div)
+    def get_comments(self, company):
+        comments = []
+        pattern = re.compile(r"(\d+(d|w))")
+        while True:
+            comments_div = self.find_elements("div.x1n2onr6.x1ye3gou.x1iorvi4.x78zum5.x1q0g3np.x1a2a7pz")
+            new_comments = []
+            for comment_div in comments_div:
+                comment_text = comment_div.get_attribute("innerText")
+                name, *comment_parts = comment_text.split("\n")
+                today_date = date.today().strftime("%Y-%m-%d")
+                match = pattern.search(comment_parts[-1])
+                if match:
+                    how_long = match.group(0)
+                    comment = "\n".join(comment_parts[:-1])  # Everything except the 'how_long' part
+                else:
+                    how_long = ""
+                    comment = "\n".join(comment_parts)  # All parts are part of the comment if no 'how_long'
+                url = ""
+                a_tags = comment_div.find_elements("tag name", "a")
+                url = a_tags[0].get_attribute("href") if a_tags else ""
+
+                new_comment = {
+                    "date": today_date,
+                    "name": name,
+                    "company": company,
+                    "comment": comment,
+                    "how_long": how_long,
+                    "url": url
+                }
+                new_comments.append(json.dumps(new_comment))
+
+            if not new_comments or set(new_comments).issubset(set(comments)):
+                break
+            comments.extend(new_comments)
             self.sleep(0.5)
 
+        with open('comments_videos.json', 'w') as file:
+            for comment in comments:
+                file.write(comment + "\n")
 
