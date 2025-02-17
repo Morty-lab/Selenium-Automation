7d8f31f53e72e7348d94b4363e6ff196d66094f6
morty_lab <aniejosephcabahug@gmail.com>
2025-02-17 11:45:14 +0800

    made a few additional changes

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from automations.FacebookScraper import FacebookScraper
-from automations.FacebookScraperVideoUC import FacebookScraperVideoUC
-from automations.FacebookScraperPostUC import FacebookScrapePostUC
+from pages.FacebookScraperVideoUC import FacebookScraperVideoUC
+from pages.FacebookScraperPostUC import FacebookScrapePostUC
+import threading
 
-if __name__ == "__main__":
-    # FacebookScraper.test_facebook_scraper()
+def run_video_scraper():
+        video_scraper = FacebookScraperVideoUC()
+        video_scraper.test_facebook_scraper()
+        video_scraper.quit()
 
-    # video_scraper = FacebookScraperVideoUC()
-    # video_scraper.test_facebook_scraper()
-    # video_scraper.quit()
-    
+def run_post_scraper():
     post_scraper = FacebookScrapePostUC()
     post_scraper.test_facebook_scraper()
     post_scraper.quit()
+
+if __name__ == "__main__":
+
+    video_thread = threading.Thread(target=run_video_scraper)
+    post_thread = threading.Thread(target=run_post_scraper)
+
+    video_thread.start()
+    post_thread.start()
+
+    video_thread.join()
+    post_thread.join()
 
